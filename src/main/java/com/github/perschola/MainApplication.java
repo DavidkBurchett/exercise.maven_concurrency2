96ce2f92db22af4a97749f7987ffeecb009226c6
David_Kyle_Burchett <davidkburchett@gmail.com>
2020-08-06 15:10:30 -0400

    part 4 done

--- src/main/java/com/github/perschola/MainApplication.java
+++ src/main/java/com/github/perschola/MainApplication.java
@@ -2,8 +2,16 @@ package com.github.perschola;
 
 public class MainApplication {
     public static void main(String[] args) {
-        (new Thread(new MyObject())).run();
-        (new Thread(new MyObject())).start();
-        (new Thread(new MyObject())).start();
+        Thread thread1 = (new Thread(new MyObject()));
+        Thread thread2 = (new Thread(new MyObject()));
+        Thread thread3 = (new Thread(new MyObject()));
+
+        thread1.setPriority(Thread.MIN_PRIORITY);
+        thread2.setPriority(Thread.NORM_PRIORITY);
+        thread3.setPriority(Thread.MAX_PRIORITY);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
     }
 }
--- src/main/java/com/github/perschola/MyObject.java
+++ src/main/java/com/github/perschola/MyObject.java
@@ -3,14 +3,29 @@ package com.github.perschola;
 public class MyObject implements Runnable {
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName());
-        for (int i = 1; i <= 5; i++) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println(i);
+
+//        --part 3--
+//        System.out.println(Thread.currentThread().getName());
+//        for (int i = 1; i <= 5; i++) {
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//            System.out.println(i);
+
+
+//        --part4--
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Thread name: " + Thread.currentThread().getName());
+            System.out.println("Thread priority: " + Thread.currentThread().getPriority());
+            System.out.println("===========================");
         }
     }
 }
